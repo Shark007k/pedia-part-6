805254c73a651c865eedb8fd9422453f09015e69
Shark007k <168632315+Shark007k@users.noreply.github.com>
2024-05-17 09:04:13 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,483 @@
+const questions = [
+    
+    {
+        question: "In a cystic fibrosis patient, infection by which of the following organisms has increased mortality?",
+        answers: [
+          { text: "Staphylococcus aureus", correct: false },
+          { text: "Pseudomonas aerogenes", correct: false },
+          { text: "Burkholderia cepacia", correct: true },
+          { text: "Aeromonas", correct: false },
+        ],
+      },
+    
+    {
+        question: "Which of the following drug(s) is an emerging treatment for Cystic fibrosis?",
+        answers: [
+          { text: "lvacaftor", correct: true },
+          { text: "Ravulizumab", correct: false },
+          { text: "Tecovirimat", correct: false },
+          { text: "All of the above", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "All the following are components of Kartagener triad except __",
+        answers: [
+          { text: "Situs inversus totals", correct: false },
+          { text: "Chronic sinusitis", correct: false },
+          { text: "Male infertility", correct: true },
+          { text: "Bronchiectasis", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "A 4-year-old girl presented with cough and difficulty breathing following accidental inhalation of a peanut. On examination, there was localiced wheezing and decreased breath sounds on the right side. What is the next best step in management?",
+        answers: [
+          { text: "Chest X-ray", correct: false },
+          { text: "Rigid bronchoscopy", correct: true },
+          { text: "Flexible endoscopy", correct: false },
+          { text: "Direct Laryngoscopy", correct: false },
+        ],
+      },
+    
+    {
+        question: "A term male neonate develops respiratory distress at birth not responding to surfactant. ECHO is normal, and the X-ray image shows ground-glass appearance. History of one-month-old male sibling death. What is the most likely diagnosis?",
+        answers: [
+          { text: "Totally anomalous pulmonary venous congestion", correct: false },
+          { text: "Meconium aspiration", correct: false },
+          { text: "Neonatal pulmonary alveolar proteinases", correct: true },
+          { text: "Diffuse herpes simplex infection", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "A 3-month old previously healthy baby was brought to the emergency department when the parents found that the baby was not breathing while asleep. Following resuscitative measures, the baby was still apnoeic, pulseless and asystolic. Which of the following is not a risk factor for sudden infant death syndrome?",
+        answers: [
+          { text: "Infants of higher birth order", correct: false },
+          { text: "Infants born to women aged >35 years", correct: true },
+          { text: "Infants of mother who smoked during pregnancy", correct: false },
+          { text: "Infants sleeping in prone position", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "All of the following are true about Macleod syndrome except that",
+        answers: [
+          { text: "It is also called Swyer-James syndrome", correct: false },
+          { text: "It is not a true emphysema.", correct: false },
+          { text: "It is mostly linked to adenovirus", correct: false },
+          { text: "Affected lung is larger in size compared to unaffected lung.", correct: true },
+        ],
+      },
+    
+    {
+        question: "The following are features of bronchial cysts except that ___ _",
+        answers: [
+          { text: "It is mostly mediastinal.", correct: false },
+          { text: "It is usually confined to 1 lobe.", correct: false },
+          { text: "It is usually multiloculated", correct: false },
+          { text: "It is not associated with malignancy.", correct: true },
+        ],
+      },
+    
+    
+    {
+        question: "All the following are components of yellow nail syndrome except _",
+        answers: [
+          { text: "Pleural effusion", correct: false },
+          { text: "Lymphedema", correct: false },
+          { text: "Clubbing", correct: true },
+          { text: "Discolored nails", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "All the following are components of Young's syndrome except __ _",
+        answers: [
+          { text: "Bronchiectasis", correct: false },
+          { text: "Asthma", correct: true },
+          { text: "Sinusitis", correct: false },
+          { text: "Obstructive azoospennia", correct: false },
+        ],
+      },
+    
+    {
+        question: "The most common mediastinal mass in children is __ _",
+        answers: [
+          { text: "Neurogenic tumors", correct: true },
+          { text: "Thymoma", correct: false },
+          { text: "Germ cell tumors", correct: false },
+          { text: "Lymphoma", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "A one-and-half-year-old child brought to the hospital with a history of breathlessness, fever, cough, and cold. On examination, respiratory rate was 46/min, and there were no chest retractions. Which is the most appropriate management?",
+        answers: [
+          { text: "Give IV antibiotics and observe", correct: false },
+          { text: "Give IV antibiotics and refer to higher centre", correct: false },
+          { text: "Oral antibiotics, warm of danger signs, and send home", correct: true },
+          { text: "No need of treatment only home remedies", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "A 31-week-pregnant woman delivers a child. In 4 hrs, the baby develops dyspnoea and cyanosis. The X-ray image of the newborn taken on day 2 and day 9 post the onset of symptoms suggests the diagnosis of _",
+        answers: [
+          { text: "Meconium aspiration syndrome", correct: false },
+          { text: "Hyaline membrane disease", correct: true },
+          { text: "Transient tachypnoea of newborn", correct: false },
+          { text: "Bronchopulmonary dysplasia", correct: false },
+        ],
+      },
+    
+    {
+        question: "The most important risk factor of neonatal respiratory distress syndrome is __ _",
+        answers: [
+          { text: "Asphyxia", correct: false },
+          { text: "Pre maturity", correct: true },
+          { text: "Twin pregnancy", correct: false },
+          { text: "Diabetic mother", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "An infant born following an elective caesarean section developed dyspnoea and cyanosis a few hours after birth. Which of the following features would favour the diagnosis of neonatal respiratory distress syndrome?",
+        answers: [
+          { text: "History of receiving antenatal corticosteroids", correct: false },
+          { text: "Air bronchogram on chest X-ray", correct: true },
+          { text: "onset of distress after 6 hours of birth", correct: false },
+          { text: "Term birth", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "All are true about the treatment of RDS with surfactant except __ _",
+        answers: [
+          { text: "In neonates with RDS who fail CPAP, treatment with endotracheal surfactant should be initiated immediately after intubation", correct: false },
+          { text: " It is given prophylactically to all neonates at < 31 weeks gestation.", correct: true },
+          { text: "It can be given as rescue treatment (when RDS actually develops).", correct: false },
+          { text: "Repeated dosing is given every 6-12 hr for a total of 2 to 4 doses.", correct: false },
+        ],
+      },
+    
+    {
+        question: "A 27-year-old primigravida presents with regular painful uterine contractions at 32 weeks of gestation. On examination, her cervix is 2cm dilated. What is the dose of antenatal betamethasone for her to prevent respiratory distress syndrome?",
+        answers: [
+          { text: "6 mg every 12 hours", correct: false },
+          { text: "12 mg every 24 hours", correct: true },
+          { text: " 6 mg stat", correct: false },
+          { text: "12 mg stat", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "Fetal lung maturity is assessed by __ _",
+        answers: [
+          { text: "L/S ratio", correct: true },
+          { text: "Bilirubin content of amniotic fluid", correct: false },
+          { text: "Ultrasound", correct: false },
+          { text: "Amniocentesis", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "A 1.5 kg child born at 32 weeks by LSCS presents with moderate respiratory difficulty (RR: 70/min, Spo2: 92%). Which is the next best step?",
+        answers: [
+          { text: "Mechanical ventilation", correct: false },
+          { text: "CPAP", correct: true },
+          { text: "100% oxygen", correct: false },
+          { text: "Surfactant and ventilation", correct: false },
+        ],
+      },
+    
+    {
+        question: "Hyaline membrane seen in the lung is composed of __ _",
+        answers: [
+          { text: "Globulin", correct: false },
+          { text: "Fibrin", correct: true },
+          { text: "Mucoprotein", correct: false },
+          { text: "Polysaccharide", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "Which of the following is true about transient tachypnea of the newborn?",
+        answers: [
+          { text: "Interlobar fissure effusion", correct: true },
+          { text: "Common in preterm infants", correct: false },
+          { text: "Air bronchogram on chest X-ray scan", correct: false },
+          { text: "Respiratory distress resolves in 2 weeks", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "Meconium passage in utero leads to ___ _",
+        answers: [
+          { text: "Listeriosis", correct: false },
+          { text: "Pathological jaundice", correct: false },
+          { text: "Obstructive emphysema", correct: true },
+          { text: "Meconium ileus", correct: false },
+        ],
+      },
+    
+    {
+        question: "Which of the following parameters is not considered in resuscitation of non-vigorous newborns with meconium-stained amniotic fluid?",
+        answers: [
+          { text: "Color", correct: true },
+          { text: "Tone", correct: false },
+          { text: "Respiratory effort", correct: false },
+          { text: "Heart rate", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "An infant in the NICU was observed to have marked nasal flare, distinct xiphoid retractions, minimal intercostal retractions, and inspiratory lag with an audible grunt. What would be the Silverman Anderson score?",
+        answers: [
+          { text: "10", correct: false },
+          { text: "7", correct: false },
+          { text: "8", correct: true },
+          { text: "9", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "Downe's score includes all except __",
+        answers: [
+          { text: "Cyanosis", correct: false },
+          { text: "Nasal flaring", correct: true },
+          { text: "Air entry", correct: false },
+          { text: "Respiratory rate", correct: false },
+        ],
+      },
+    
+    {
+        question: "A 3-hour-old neonate with apnea is on bag and mask for last 30 seconds, now showing spontaneous breathing with a heart rate of 110/min. The next step would be",
+        answers: [
+          { text: "Discontinue ventilation", correct: true },
+          { text: "Continue ventilation", correct: false },
+          { text: "Give chest compression", correct: false },
+          { text: "ET intubation", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "All of the following are causes of bronchopulmonary dysplasia except ___ _",
+        answers: [
+          { text: "Oxygen toxicity", correct: false },
+          { text: "Traumatic damage to lungs", correct: false },
+          { text: "Theophylline use", correct: true },
+          { text: "Pulmonary edema due to capillary damage", correct: false },
+        ],
+      },
+    
+    
+    
+     {
+        question: "All are true about congenital diaphragmatic hernia except __ _",
+        answers: [
+          { text: "Not associated with dextrocardia", correct: true },
+          { text: "Most commonly seen on the left side", correct: false },
+          { text: "Mortality is usually associated with pulmonary hypoplasia", correct: false },
+          { text: "Pulmonary dilators markedly improve the survival", correct: false },
+        ],
+      },
+    
+    {
+        question: "A newborn baby has been referred to the casualty as a case of congenital diaphragmatic hernia. Which of the following is an initial clinical intervention?",
+        answers: [
+          { text: "Intubate and insert a nasogastric tube", correct: true },
+          { text: "Bag and mask ventilation", correct: false },
+          { text: "Insert a central venous pressure line", correct: false },
+          { text: "Ventilate with a high-frequency ventilator", correct: false },
+        ],
+      },
+    
+    
+    {
+        question: "The major limiting factor for survival in congenital diaphragmatic hernia is __",
+        answers: [
+          { text: "Septicemia", correct: false },
+          { text: "Hemorrhage", correct: false },
+          { text: "Intestinal obstruction", correct: false },
+          { text: "Pulmonary hypoplasia", correct: true },
+        ],
+      },
+    
+     {
+        question: "All of the following are true about apnea of prematurity (AOP) except __ _",
+        answers: [
+          { text: "Usually resolves by 37 weeks post conceptional age", correct: false },
+          { text: "Gentle tactile stimulation is often adequate for mild to moderate episodes", correct: false },
+          { text: "Recurrent AOP can be treated with Caffeine or theophylline", correct: false },
+          { text: "AOP is a risk factor for Sudden infant death syndrome (SIDS)", correct: true },
+        ],
+      },
+    
+    {
+        question: "The chest X-ray image of a term baby born with meconium aspiration shows ____",
+        answers: [
+          { text: "Mediastinal shift", correct: false },
+          { text: "Ground glass opacities", correct: false },
+          { text: "Fluid in the interlobar fissure", correct: false },
+          { text: "Patchy infiltrates and coarse streaking", correct: true },
+        ],
+      },
+    
+    
+    {
+        question: "Which of the following is not an indicator of fetal lung maturity?",
+        answers: [
+          { text: "Lecithin-sphingomyelin ratio> 2", correct: false },
+          { text: "Positive shake test", correct: false },
+          { text: "Increased phosphatidyl glycerol", correct: false },
+          { text: "Nile blue test showing >50% of blue cells", correct: true },
+        ],
+      },
+    
+    
+    
+    
+    
+    
+    
+      
+]
+
+
+
+
+
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const backButton = document.getElementById("back-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz (){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    backButton.innerHTML = "Back";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    
+    if(answer.correct){
+        button.dataset.correct = answer.correct;  
+    }
+    button.addEventListener("click", selectAnswer)
+
+})
+}
+
+
+function resetState(){
+nextButton.style.display = "none";
+backButton.style.display = "none";
+
+while(answerButtons.firstChild){
+answerButtons.removeChild(answerButtons.firstChild);
+
+}
+}
+
+function selectAnswer(e){
+
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else{
+    selectedBtn.classList.add("inCorrect")
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "inline";
+    backButton.style.display = "inline";
+
+}
+
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length} !`;
+    nextButton.innerHTML = "Solve Again";
+    nextButton.style.display = "inline"
+    backButton.style.display = "inline"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else { showScore()}
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length) {
+    handleNextButton();
+    } else{
+        startQuiz();
+    }
+});
+
+function handlebackButton(){
+  currentQuestionIndex--;
+  if(currentQuestionIndex < questions.length){
+      showQuestion();
+  } else { showScore()}
+}
+
+backButton.addEventListener("click", ()=>{
+  if(currentQuestionIndex < questions.length) {
+  handlebackButton();
+  } else{
+      startQuiz();
+  }
+});
+startQuiz();
